a3c8fa549f7cbfbcf2d93f96f16ae93724d0cb40
Connor Davis <connor.davis@thusa.co.za>
2025-09-08 00:35:33 +0200

    feat(transactions): enhance transaction schemas with seller and buyer properties and update material schemas

--- internal/routing/properties/transaction.go
+++ internal/routing/properties/transaction.go
@@ -8,9 +8,32 @@ var TransactionProperties = map[string]*openapi3.Schema{
 	"updatedAt": openapi3.NewDateTimeSchema(),
 }
 
+var CreateTransactionProperties = map[string]*openapi3.Schema{
+	"sellerId": openapi3.NewUUIDSchema(),
+	"buyerId":  openapi3.NewUUIDSchema(),
+}
+
+var UpdateTransactionProperties = map[string]*openapi3.Schema{
+	"sellerId": openapi3.NewUUIDSchema().WithNullable(),
+	"buyerId":  openapi3.NewUUIDSchema().WithNullable(),
+}
+
 var TransactionMaterialProperties = map[string]*openapi3.Schema{
 	"id":        openapi3.NewUUIDSchema(),
 	"weight":    openapi3.NewFloat64Schema(),
+	"value":     openapi3.NewFloat64Schema(),
 	"createdAt": openapi3.NewDateTimeSchema(),
 	"updatedAt": openapi3.NewDateTimeSchema(),
+}
+
+var CreateTransactionMaterialProperties = map[string]*openapi3.Schema{
+	"materialId": openapi3.NewUUIDSchema(),
+	"weight":     openapi3.NewFloat64Schema(),
+	"value":      openapi3.NewFloat64Schema(),
+}
+
+var UpdateTransactionMaterialProperties = map[string]*openapi3.Schema{
+	"materialId": openapi3.NewUUIDSchema().WithNullable(),
+	"weight":     openapi3.NewFloat64Schema().WithNullable(),
+	"value":      openapi3.NewFloat64Schema().WithNullable(),
 }
--- internal/routing/schemas/transaction.go
+++ internal/routing/schemas/transaction.go
@@ -18,18 +18,39 @@ var TransactionMaterialSchema = openapi3.NewSchema().
 
 var TransactionMaterialsArraySchema = openapi3.NewArraySchema().WithItems(TransactionMaterialSchema.Value).NewRef()
 
+var CreateTransactionMaterialSchema = openapi3.NewSchema().
+	WithProperties(properties.CreateTransactionMaterialProperties).
+	WithRequired([]string{
+		"materialId",
+		"weight",
+		"value",
+	}).NewRef()
+
+var UpdateTransactionMaterialSchema = openapi3.NewSchema().
+	WithProperties(properties.UpdateTransactionMaterialProperties).NewRef()
+
 var TransactionSchema = openapi3.NewSchema().
 	WithProperties(properties.TransactionProperties).
-	WithProperty("collector", UserSchema.Value).
-	WithProperty("organization", OrganizationSchema.Value).
+	WithProperty("seller", OrganizationSchema.Value).
+	WithProperty("buyer", OrganizationSchema.Value).
 	WithProperty("materials", TransactionMaterialsArraySchema.Value).
 	WithRequired([]string{
 		"id",
-		"collector",
-		"organization",
+		"seller",
+		"buyer",
 		"materials",
 		"createdAt",
 		"updatedAt",
 	}).NewRef()
 
 var TransactionsSchema = openapi3.NewArraySchema().WithItems(TransactionSchema.Value).NewRef()
+
+var CreateTransactionSchema = openapi3.NewSchema().
+	WithProperties(properties.CreateTransactionProperties).
+	WithRequired([]string{
+		"sellerId",
+		"buyerId",
+	}).NewRef()
+
+var UpdateTransactionSchema = openapi3.NewSchema().
+	WithProperties(properties.UpdateTransactionProperties).NewRef()
