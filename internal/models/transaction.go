7061299f2c485a0ec975c57f686ff4f3b13e3cab
Connor Davis <connor.davis@thusa.co.za>
2025-09-08 00:33:18 +0200

    feat(transactions): add seller and buyer fields to Transaction model and corresponding payloads

--- internal/models/transaction.go
+++ internal/models/transaction.go
@@ -4,11 +4,21 @@ import "github.com/google/uuid"
 
 type Transaction struct {
 	Base
-	Materials      []TransactionMaterial `json:"materials" gorm:"foreignKey:TransactionId;references:Id;constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"`
-	CollectorId    uuid.UUID             `json:"-" gorm:"type:uuid;not null"`
-	Collector      User                  `json:"collector" gorm:"foreignKey:CollectorId;references:Id;constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"`
-	OrganizationId uuid.UUID             `json:"-" gorm:"type:uuid;not null"`
-	Organization   Organization          `json:"organization" gorm:"foreignKey:OrganizationId;references:Id;constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"`
+	Materials []TransactionMaterial `json:"materials" gorm:"foreignKey:TransactionId;references:Id;constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"`
+	SellerId  uuid.UUID             `json:"-" gorm:"type:uuid;not null"`
+	Seller    Organization          `json:"seller" gorm:"foreignKey:SellerId;references:Id;constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"`
+	BuyerId   uuid.UUID             `json:"-" gorm:"type:uuid;not null"`
+	Buyer     Organization          `json:"buyer" gorm:"foreignKey:BuyerId;references:Id;constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"`
+}
+
+type CreateTransactionPayload struct {
+	SellerId uuid.UUID `json:"sellerId"`
+	BuyerId  uuid.UUID `json:"buyerId"`
+}
+
+type UpdateTransactionPayload struct {
+	SellerId *uuid.UUID `json:"sellerId"`
+	BuyerId  *uuid.UUID `json:"buyerId"`
 }
 
 type TransactionMaterial struct {
@@ -18,4 +28,16 @@ type TransactionMaterial struct {
 	Material      Material  `json:"material" gorm:"foreignKey:MaterialId;references:Id;constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"`
 	Weight        float64   `json:"weight" gorm:"type:decimal(10,2);not null"`
 	Value         float64   `json:"value" gorm:"type:decimal(10,2);not null"`
+}
+
+type CreateTransactionMaterialPayload struct {
+	MaterialId uuid.UUID `json:"materialId"`
+	Weight     float64   `json:"weight"`
+	Value      float64   `json:"value"`
+}
+
+type UpdateTransactionMaterialPayload struct {
+	MaterialId *uuid.UUID `json:"materialId"`
+	Weight     *float64   `json:"weight"`
+	Value      *float64   `json:"value"`
 }
